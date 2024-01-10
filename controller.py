c688ca440e23db04f8a65156e43773744e1f5def
Thomas D <thomas.dumond@etu.unistra.fr>
2024-01-10 14:33:01 +0000

    ask user topology

---
+++ controller.py
@@ -0,0 +1,7 @@
+from p4utils.utils.helper import load_topo
+from p4utils.utils.sswitch_p4runtime_API import SimpleSwitchP4RuntimeAPI
+from p4utils.utils.sswitch_thrift_API import SimpleSwitchThriftAPI
+
+
+topo = load_topo('topology.json')
+controllers = {}
--- network.py
+++ network.py
@@ -4,5 +4,56 @@ net = NetworkAPI()
 
 # General settings
 net.setLogLevel('info')
+net.setCompiler(p4rt=True)
+net.execScript('python controller.py', reboot=True)
 
 # Network definition
+availables_equipment = ['firewall', 'router-controller','touter-lw-controller', 'load-balancer', 'host']
+
+# Ask for the equipement running on each node
+node_number = int(input("Nombre de noeuds: "))
+
+for i in range(node_number):
+    node_name = input("Nom du noeud: ")
+    node_type = input("Type du noeud: (firewall, router-controller, router-lw-controller, load-balancer, host)")
+    if node_type in availables_equipment:
+        if node_type == 'firewall':
+            net.addP4Switch(node_name)
+            net.setP4Source(node_name, 'firewall.p4')
+            
+        elif node_type == 'router-controller':
+            net.addP4Switch(node_name)
+            net.setP4Source(node_name, 'router-controller.p4')
+            
+        elif node_type == 'router-lw-controller':
+            net.addP4Switch(node_name)
+            net.setP4Source(node_name, 'router-lw-controller.p4')
+            
+        elif node_type == 'load-balancer':
+            net.addP4Switch(node_name)
+            net.setP4Source(node_name, 'load-balancer.p4')
+            
+        elif node_type == 'host':
+            net.addHost(node_name)
+            
+    else:
+        print("Type de noeud invalide")
+        exit(1)
+
+# Ask for the links between the nodes
+link_number = int(input("Nombre de liens: "))
+
+for i in range(link_number):
+    node1 = input("Nom du premier noeud: ")
+    node2 = input("Nom du deuxième noeud: ")
+    net.addLink(node1, node2) #TODO: User port for each link
+    
+    
+#Assignment strategy
+net.l3()   
+    
+# Node general options
+net.enablePcapDumpAll()
+net.enableLogAll()
+net.enableCli()
+net.startNetwork()
