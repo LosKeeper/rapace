6d27d626ee488bd601a5e1fa0a46c245781d2baf
Thomas D <thomas.dumond@etu.unistra.fr>
2024-01-10 13:57:52 +0000

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# RAPACE
+
+## Auteurs
+- Thomas DUMOND
+- Ethan HURET
+
+## [Sujet](sujet.pdf)
+
+## Patch
+Avant de commencer, il est nécessaire de patcher p4
+```bash
+cd patch
+./patch.sh
+```
---
+++ patch/patch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -f /home/p4/p4-tools/p4-utils/p4utils/utils/thrift_API.py
+cp thrift_API.py /home/p4/p4-tools/p4-utils/p4utils/utils/thrift_API.py
+
+rm -f /home/p4/p4-tools/p4-utils/p4utils/node.py
+cp node.py /home/p4/p4-tools/p4-utils/p4utils/node.py
